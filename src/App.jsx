0cac2b17aa06160209c14feb80a23be7c983be98
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-02-06 16:01:42 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Dropdown from "./Dropdown";
+import { DropdownProvider } from "./DropdownContext";
 
 const items = ["첫 번째 아이템", "두 번째 아이템", "세 번째 아이템"];
 
 function App() {
   return (
-    <div>
+    <DropdownProvider>
       <h3>useContext로 만드는 Dropdown Item</h3>
+
       <Dropdown items={items} />
-    </div>
+    </DropdownProvider>
   );
 }
 
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import { DropdownProvider, useDropdown } from "./DropdownContext";
 
-const DropdownButton = () => {
-  return <button>Dropdown Button</button>;
+const DropdownButton = ({ toggleDropdown, selectedItem }) => {
+  return (
+    <button onClick={toggleDropdown}>
+      {selectedItem ? selectedItem : "Dropdown Button"}
+    </button>
+  );
 };
 
-const DropdownMenu = ({ items }) => {
+const DropdownMenu = ({ items, selectedItem, selectItem, toggleDropdown }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li
+          onClick={() => {
+            selectItem(item);
+            toggleDropdown(false);
+          }}
+          key={item}
+        >
+          {item === selectedItem ? item : item}
+        </li>
       ))}
     </ul>
   );
 };
 
 const Dropdown = ({ items }) => {
+  const { isOpen, selectedItem, toggleDropdown, selectItem } = useDropdown();
   return (
     <DropdownProvider>
       <div>
-        <DropdownButton />
-        <DropdownMenu items={items} />
+        <DropdownButton
+          selectedItem={selectedItem}
+          toggleDropdown={toggleDropdown}
+        />
+        {isOpen && (
+          <DropdownMenu
+            selectItem={selectItem}
+            items={items}
+            selectedItem={selectedItem}
+            toggleDropdown={toggleDropdown}
+          />
+        )}
       </div>
     </DropdownProvider>
   );
